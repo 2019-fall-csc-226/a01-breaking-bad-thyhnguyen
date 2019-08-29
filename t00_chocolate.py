c33ae3fa9a617fc63a388648f8e3da1b4f4a6f5c
nguyent2 <nguyent2@berea.edu>
2019-08-28 20:15:22 -0400

    Change Author, username

--- t00_chocolate.py
+++ t00_chocolate.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Scott Heggen & Emily Lovell           ****** TODO: CHANGE THIS!! ******
-# username: heggens & lovelle             ****** TODO: CHANGE THIS!! *****
+# Author: Thy H. Nguyen          ****** TODO: CHANGE THIS!! ******
+# username: nguyent2             ****** TODO: CHANGE THIS!! *****
 #
 # Purpose: Designed to compute the total chocolate desired by the user
 #
