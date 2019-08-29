3b37b53941906ad56ffb896307a4366110c0b0ed
nguyent2 <nguyent2@berea.edu>
2019-08-28 23:47:09 -0400

    Task 2 completed in a completely different way than task 1: Asking for the user's friend birth year and give the Chinese Zodiac

--- a01_stubs.py
+++ a01_stubs.py
@@ -55,9 +55,39 @@ else:
 ######################################################################
 # (Required) Task 2
 # TODO Ask the user for their friend's birth year
+print()
+print ("Now, you have known your Chinese Zodiac animal. What about your friends? \n Are you curious about your friend's Chinese Zodiac animal?")
+birth_year = (input("What is your friend's year of birth? [Make sure that you remember to enter a year between 2000 and 2011] "))
+print()
+yearr = int(birth_year[-2:])
+if yearr == 00:
+    print ("Your friend's Chinese Zodiac animal is a Dragon.")
+elif yearr == 1:
+    print ("Your friend's Chinese Zodiac animal is a Snake.")
+elif yearr == 2:
+    print("Your friend's Chinese Zodiac animal is a Horse.")
+elif yearr == 3:
+    print("Your friend's Chinese Zodiac animal is a Goat.")
+elif yearr == 4:
+    print("Your friend's Chinese Zodiac animal is a Monkey.")
+elif yearr == 5:
+    print("Your friend's Chinese Zodiac animal is a Rooster.")
+elif yearr == 6:
+    print("Your friend's Chinese Zodiac animal is a Dog.")
+elif yearr == 7:
+    print("Your friend's Chinese Zodiac animal is a Pig.")
+elif yearr == 8:
+    print("Your friend's Chinese Zodiac animal is a Rat.")
+elif yearr == 9:
+    print("Your friend's Chinese Zodiac animal is a Ox.")
+elif yearr == 10:
+    print("Your friend's Chinese Zodiac animal is a Tiger.")
+elif yearr == 11:
+    print("Your friend's Chinese Zodiac animal is a Rabbit.")
+else:
+    print("Did you remember to enter a birth year between 2000 and 2011? \n Please try again!")
 
-
-# TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
+    # TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
 
 
 ######################################################################
